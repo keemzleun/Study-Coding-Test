4e57dca6a9cad7fcb7944f08c78c0e5d2d97a93c
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-04-15 17:20:12 +0900

    24/04/15

--- src/Baekjoon/Boj1940.java
+++ src/Baekjoon/Boj1940.java
@@ -21,8 +21,8 @@ public class Boj1940 {
         int j = N-1;
 
         while(i<j) {
-            if (A[i] + A[j] < M) i++;
-            else if(A[i] + A[j] > M) j--;
+            if (A[i] + A[j] < M) i++;   // 합이 M보다 작을 시 가장 작은 수 A[i]를 증가시키기
+            else if(A[i] + A[j] > M) j--;  // 합이 M보다 클 시 가장 큰 수 A[j]를 감소시키기
             else {
                 count++;
                 i++; j--;
