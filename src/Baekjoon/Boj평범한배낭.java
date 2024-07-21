678cfbb1f2dc50b0ecea25004478266753772512
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-07-21 13:11:51 +0900

    java

--- src/Baekjoon/Boj평범한배낭.java
+++ src/Baekjoon/Boj평범한배낭.java
@@ -1,9 +1,6 @@
 package Baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Boj평범한배낭 {
 
@@ -11,51 +8,46 @@ public class Boj평범한배낭 {
     static int[] W; // weight
     static int[] V; // value
 
-    public static void main(String[] args) throws IOException {
-        // N : 물건의 개수
-        // W : 물건의 무게
-        // V : 물건의 가치
-        // K : 최대 무게
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        // 물품의 수
+        int n = sc.nextInt();
+        // 버틸 수 있는 무게
+        int k = sc.nextInt();
+        // 물품의 무게(0)와 가치(1)
+        int[][] a = new int[n+1][2];
 
-        W = new int[N];
-        V = new int[N];
+        // 행: 물품의 무게, 열: 1~k까지의 무게, 값: 물품의 가치 누적
+        int[][] dp = new int[n+1][k+1];
 
-        dp = new Integer[N][K + 1];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            W[i] = Integer.parseInt(st.nextToken());
-            V[i] = Integer.parseInt(st.nextToken());
+        // 물품들의 무게와 가치 입력
+        for(int i=1;i<n+1;i++) {
+            a[i][0] = sc.nextInt();
+            a[i][1] = sc.nextInt();
         }
 
-        System.out.println(knapsack(N - 1, K));
-
-    }
-
-    static int knapsack(int i, int k) {
-        // i가 0미만, 즉 범위 밖이 된다면
-        if (i < 0)
-            return 0;
-
-        // 탐색하지 않은 위치라면?
-        if (dp[i][k] == null) {
-
-            // 현재 물건(i)을 추가로 못담는 경우 (이전 i값 탐색)
-            if(W[i] > k) {
-                dp[i][k] = knapsack(i - 1, k);
-            }
-            // 현재 물건(i)을 담을 수 있는 경우
-            else {
-                // 이전 i값과 이전 i값에 대한 k-W[i]의 값 + 현재 가치(V[i])중 큰 값을 저장
-                dp[i][k] = Math.max(knapsack(i - 1, k), knapsack(i - 1, k - W[i]) + V[i]);
+        // 0행, 0열은 공백 인덱스로 비워둔다.
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<k+1;j++) {
+                // 비교 무게 - 물품의 무게 >= 0 인 경우,
+                if(j - a[i][0] >= 0) {
+                    // 이전 dp에 저장된 누적 가치와 자신의 가치+남은 무게의 가치 중 큰 값을 취한다.
+                    // 점화식은 기본적으로 dp[i][j]의 값은 dp [i-1][j]의 값을 가진다.
+                    // 왜냐하면, 2번 아이템의 경우 무게가 2일 때까지 2번 아이템은 저장하지 못하므로, 1번 아이템에서 구해놓은 가치를 저장해둔다.
+                    dp[i][j] = Math.max( dp[i-1][j], a[i][1]+dp[i-1][j-a[i][0]]);
+                }else {
+                    // 나머지는 이전 dp에 누적된 가치를 취한다.
+                    dp[i][j] = dp[i-1][j];
+                }
             }
         }
-        return dp[i][k];
+
+        System.out.println(dp[n][k]);
+
+        sc.close();
     }
+
 }
+
