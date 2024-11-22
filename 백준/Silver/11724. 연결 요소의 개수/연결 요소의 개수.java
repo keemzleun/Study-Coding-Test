08e703abeff942f1154b432e88ac29e2f9c11560
김지은 <92363679+keemzleun@users.noreply.github.com>
2024-11-22 13:57:44 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 620 ms, Memory: 142408 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,55 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+// 연결 요소의 개수
+public class Main {
+    static List<List<Integer>> adjList;
+    static boolean[] visited;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        adjList = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+
+        visited = new boolean[N+1];
+        count = 0;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static void dfs(int start){
+        visited[start] = true;
+        for (int target : adjList.get(start)) {
+            if (!visited[target]) {
+                dfs(target);
+            }
+        }
+    }
+}
