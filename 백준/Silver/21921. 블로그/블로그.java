28155d28be34b2c5c56578ea2bfb14343e4e6acf
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-02-07 15:20:26 +0900

    [Silver III] Title: 블로그, Time: 316 ms, Memory: 35240 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -6,39 +6,39 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
+        int[] visitors = new int[N];
 
-        int[] visit = new int[N];
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            visit[i] = Integer.parseInt(st.nextToken());
+            visitors[i] = Integer.parseInt(st.nextToken());
         }
 
-        int max = 0;
-        int count = 0;
+        // 첫 번째 구간 합 계산
         int sum = 0;
-
-        for (int i = 0; i < X; i++){
-            sum += visit[i];
+        for (int i = 0; i < X; i++) {
+            sum += visitors[i];
         }
-        max = sum;
-        count = 1;
 
-        // 슬라이딩 윈도우
+        int max = sum;
+        int count = 1;
+
         for (int i = X; i < N; i++) {
-            sum = sum - visit[i - X] + visit[i];
-
+            sum += visitors[i];     // 새로운 값을 더하고
+            sum -= visitors[i - X]; // 오래된 값을 빼줌
+            
             if (sum > max) {
                 max = sum;
-                count = 1;
+                count = 1;  // 새로운 최대값이 나오면 count 초기화
             } else if (sum == max) {
                 count++;
             }
         }
 
-        if (max == 0){
+        if (max == 0) {
             System.out.println("SAD");
         } else {
             System.out.println(max);
