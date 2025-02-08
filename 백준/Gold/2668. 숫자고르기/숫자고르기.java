71e0fc262b6a8c8d49b00c956be5e1ccaf278b37
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-02-08 22:01:32 +0900

    [Gold V] Title: 숫자고르기, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.java
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -1,23 +1,26 @@
-import java.util.*;
-import java.io.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+// 숫자고르기
 public class Main {
     static int[] arr;
     static ArrayList<Integer> list;
     static boolean[] visit;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        arr = new int[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        arr = new int[N+1];
+        for (int i = 1; i < N+1; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-
         list = new ArrayList<>();
         visit = new boolean[N + 1];
-        for (int i = 1; i < N + 1; i++) {
+        for (int i = 1; i < N+1; i++) {
             visit[i] = true;
             dfs(i, i);
             visit[i] = false;
@@ -26,17 +29,16 @@ public class Main {
         Collections.sort(list);
         System.out.println(list.size());
         list.stream().forEach(System.out::println);
+        
     }
-
-    private static void dfs(int idx, int st) {
-        if (arr[idx] == st) {
-            list.add(st);
+    static void dfs(int index, int start){
+        if (arr[index] == start){
+            list.add(start);
         }
-
-        if (!visit[arr[idx]]) {
-            visit[arr[idx]] = true;
-            dfs(arr[idx], st);
-            visit[arr[idx]] = false;
+        if (!visit[arr[index]]){
+            visit[arr[index]] = true;
+            dfs(arr[index], start);
+            visit[arr[index]] = false;
         }
     }
 
