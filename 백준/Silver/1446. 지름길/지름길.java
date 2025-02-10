c10fe03ecd34e873d20091d136cec060a87da65b
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-02-10 23:06:43 +0900

    [Silver I] Title: 지름길, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.java
+++ 백준/Silver/1446. 지름길/지름길.java
@@ -1,41 +1,43 @@
-import java.util.*;
-import java.io.*;
-import java.math.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+// 지름길
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int sum = 0;
+        int[][] arr = new int[N][3];
 
-        int[][] arr = new int[n][3];
-
-        for(int i = 0;i<n;i++){
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0;j<3;j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][2] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[m+1];
+        int[] dp = new int[D+1];
 
-        for(int i = 0;i<=m;i++){
+        for(int i = 0;i <= D; i++){
             dp[i] = i;
         }
 
-        for(int i = 1;i<=m;i++){
+        for(int i = 1;i <= D;i++){
 
             dp[i] = Math.min(dp[i], dp[i-1] +1);
-            for(int j = 0;j<n;j++){
+            for(int j = 0;j < N;j++){
                 if(arr[j][1] == i){
                     dp[i] = Math.min(dp[i], dp[arr[j][0]] + arr[j][2]);
                 }
             }
         }
 
-        System.out.println(dp[m]);
+        System.out.println(dp[D]);
 
     }
 }
