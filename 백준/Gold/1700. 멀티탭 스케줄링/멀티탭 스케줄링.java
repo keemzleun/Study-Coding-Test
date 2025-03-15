6231df4b99bc43d803e87ee57ca57a7954d3bb59
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-03-15 13:48:06 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 108 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.java
@@ -1,9 +1,8 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
+// 멀티탭 스케줄링
 public class Main {
-    // 그리디
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -11,39 +10,39 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int[] order = new int[K];
+        int[] order = new int[K]; 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < K; i++) {
             order[i] = Integer.parseInt(st.nextToken());
         }
 
-        boolean[] use = new boolean[101];
+        boolean[] use = new boolean[K+1];
         int put = 0;
         int ans = 0;
         for (int i = 0; i < K; i++) {
             int temp = order[i];
 
-            if (!use[temp]) { // 콘센트가 꽂혀있지 않은 경우
-                if (put < N) { // 콘센트를 꽂을 공간이 있는 경우
+            if (!use[temp]) {
+                if (put < N) {
                     use[temp] = true;
                     put++;
-                } else { // 콘센트를 꽂을 공간이 없는 경우
+                } else {
                     ArrayList<Integer> arrList = new ArrayList<>();
-                    for (int j = i; j < K; j++) { // 현재 꽂혀 있는 콘센트가 나중에도 사용되는지 확인.
+                    for (int j = i; j < K; j++) {
                         if (use[order[j]] && !arrList.contains(order[j])) {
                             arrList.add(order[j]);
                         }
                     }
 
-                    if (arrList.size() != N) { // 나중에도 사용되는 콘센트가 구멍의 개수보다 작을 경우.
+                    if (arrList.size() != N) {
                         for (int j = 0; j < use.length; j++) {
-                            if (use[j] && !arrList.contains(j)) { // 그 콘센트를 제거.
+                            if (use[j] && !arrList.contains(j)) {
                                 use[j] = false;
                                 break;
                             }
                         }
-                    } else { // 현재 꽂혀 있는 모든 콘센트가 나중에도 사용될 경우
-                        int remove = arrList.get(arrList.size() - 1); // 가장 마지막에 사용될 콘센트를 제거.
+                    } else {
+                        int remove = arrList.get(arrList.size() - 1);
                         use[remove] = false;
                     }
 
@@ -53,7 +52,6 @@ public class Main {
             }
         }
 
-        System.out.println(ans + "\n");
-        br.close();
+        System.out.println(ans);
     }
 }
