29c19bbde6017291a21526cdd64146c773a4de0b
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-05-11 21:37:08 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14720 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,54 +1,62 @@
-import java.awt.Point;
-import java.util.*;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+// 미로 탐색
 public class Main {
-
     static int n, m;
-    static int[][] arr;
+    static int[][] map;
     static boolean[][] visited;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    static int min;
+    static int count;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        arr = new int[n][m];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
         visited = new boolean[n][m];
-        sc.nextLine();
-        
+        min = Integer.MAX_VALUE;
+
         for (int i = 0; i < n; i++) {
-            String line = sc.next();
+            String str = br.readLine();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = line.charAt(j) - '0';
+                map[i][j] = str.charAt(j) - '0';
             }
         }
 
         bfs(0, 0);
-        System.out.println(arr[n - 1][m - 1]);
+
     }
-    
-    static void bfs(int x, int y) {
-        Queue<Point> queue = new LinkedList<>();
-        queue.offer(new Point(x, y));
+
+    public static void bfs(int x, int y){
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(x, y));
         visited[x][y] = true;
 
-        while (!queue.isEmpty()) {
-            Point currentPoint = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nextX = currentPoint.x + dx[i];
-                int nextY = currentPoint.y + dy[i];
+        while (!q.isEmpty()) {
+            Point pt = q.poll();
 
-                if (nextX < 0 || nextX >= n || nextY < 0 || nextY >= m)
-                    continue;
-                if (arr[nextX][nextY] == 0)
-                    continue;
-                if (visited[nextX][nextY])
-                    continue;
-                queue.offer(new Point(nextX, nextY));
-                visited[nextX][nextY] = true;
-                arr[nextX][nextY] = arr[currentPoint.x][currentPoint.y] + 1;
+            for (int i = 0; i < 4; i++) {
+                int nx = pt.x + dx[i];
+                int ny = pt.y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && map[nx][ny] != 0 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    map[nx][ny] = map[pt.x][pt.y] + 1;
+                    q.add(new Point(nx, ny));
+                }
             }
+
         }
+        System.out.println(map[n-1][m-1]);
     }
 }
