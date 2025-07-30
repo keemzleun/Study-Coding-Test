8782badc65897469c5fef41b552f7618ff04d8a8
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-07-30 21:49:59 +0900

    [level 2] Title: 기능개발, Time: 3.39 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,28 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> result = new ArrayList<>();
-        
-        int n = progresses.length;
-        int day = 0; 
-        int count = 0; 
+        Queue<Integer>  q = new LinkedList<>();
 
-        for (int i = 0; i < n; i++) {
-            int needDay = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
-            
-            if (day < needDay) { 
-                if (count > 0) {
-                    result.add(count);
-                }
-                day = needDay;
-                count = 1;
-            } else {
+        for (int i = 0; i < progresses.length; i++) {
+            int remaining = 100 - progresses[i];    // 남은 작업량
+            int days = remaining / speeds[i];       // 소요일수
+            if (remaining % speeds[i] != 0) days++;
+            q.offer(days);
+        }
+
+        ArrayList<Integer> deployments = new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int first = q.poll();
+            int count = 1;
+
+            while(!q.isEmpty() && q.peek() <= first){
+                q.poll();
                 count++;
             }
-        }
-        
-        result.add(count); 
 
-        return result.stream().mapToInt(i -> i).toArray();
+            deployments.add(count);
+        }
+
+        return deployments.stream().mapToInt(Integer::intValue).toArray();
     }
 }
