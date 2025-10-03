0c0c6d56ddd1ae883c73e234ba5e0850b0b704ed
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-10-03 19:50:01 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.54 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,48 +1,33 @@
-// "."을 기준으로 년/월/일
-// MM이 12를 넘을 경우, YYYY는 증가하고 MM은 1이 되어야 함
-// privacy의 약관 종류 판단
 import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answer = new ArrayList<>();
-        StringTokenizer st = new StringTokenizer(today, ".");
-        int todayY = Integer.parseInt(st.nextToken());
-        int todayM = Integer.parseInt(st.nextToken());
-        int todayD = Integer.parseInt(st.nextToken());
+        
+        int date = getDate(today);
         
         Map<String, Object> map = new HashMap<String, Object>();
-        for(String t : terms){
-            st = new StringTokenizer(t);
-            String type = st.nextToken();
-            int term = Integer.parseInt(st.nextToken());
-            map.put(type, term);
+        for (String t : terms){
+            String[] term = t.split(" ");
+            map.put(term[0], Integer.parseInt(term[1]));
         }
         
-        for(int i = 0; i < privacies.length; i++){
-            st = new StringTokenizer(privacies[i]);
-            String date = st.nextToken();
-            String type = st.nextToken();
+        for (int i = 0; i < privacies.length; i++){
+            String[] privacy = privacies[i].split(" ");
             
-            st = new StringTokenizer(date, ".");
-            int YYYY = Integer.parseInt(st.nextToken());
-            int MM = Integer.parseInt(st.nextToken());
-            int DD = Integer.parseInt(st.nextToken());
+            int num = getDate(privacy[0]);
+            int type = (int)map.get(privacy[1]);
             
-            MM += (int) map.get(type); // num 대신 바로 계산
-            // 1~12월 사이클을 맞추기 위해 1을 빼고 계산 후 다시 1을 더해줌
-            YYYY += (MM - 1) / 12;
-            MM = (MM - 1) % 12 + 1;
-            
-            if (todayY > YYYY) {
-                answer.add(i+1);
-            } else if (YYYY == todayY && todayM > MM){
-                answer.add(i+1);
-            } else if (YYYY == todayY && MM == todayM && todayD >= DD){
-                answer.add(i+1);
-            }
-            
+            num += type * 28;
+            if (date >= num) answer.add(i+1);      
         }
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(integer->integer).toArray();
+    }
+    public int getDate(String today){
+        String[] dates = today.split("\\.");
+        int yyyy = Integer.parseInt(dates[0]);
+        int mm = Integer.parseInt(dates[1]);
+        int dd = Integer.parseInt(dates[2]);
+        return (yyyy * 12 * 28) + (mm * 28) + dd;
     }
 }
