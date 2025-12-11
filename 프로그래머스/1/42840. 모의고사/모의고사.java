1c3831c497c33128e7b92c44899327da25e273cb
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-12-11 12:31:12 +0900

    [level 1] Title: 모의고사, Time: 0.96 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int[] answers) {
-        int[] first = {1, 2, 3, 4, 5};
-        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        int score1 = 0; 
-        int score2 = 0; 
-        int score3 = 0;
-        
-        for (int i=0; i < answers.length; i++){
-            if (first[i % first.length] == answers[i]) score1++;
-            if (second[i % second.length] == answers[i]) score2++;
-            if (third[i % third.length] == answers[i]) score3++;
-        }
-        int maxScore = Math.max(score1, Math.max(score2, score3));
-        
-        List<Integer> topScorers = new ArrayList<>();
-        
-        if (score1 == maxScore) {
-            topScorers.add(1);
-        }
-        if (score2 == maxScore) {
-            topScorers.add(2);
-        }
-        if (score3 == maxScore) {
-            topScorers.add(3);
-        }
-        
-        int[] answer = new int[topScorers.size()];
-        for (int i = 0; i < topScorers.size(); i++) {
-            answer[i] = topScorers.get(i);
-        }
-        
+        int[] answer = {};
         return answer;
     }
 }
