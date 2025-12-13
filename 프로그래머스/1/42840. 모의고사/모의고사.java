a872cd56b98b180c309a051501ad0e76dfa85653
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-12-13 10:20:22 +0900

    [level 1] Title: 모의고사, Time: 3.04 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,6 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = {};
+        
+        int[] first = {1, 2, 3, 4, 5};
+        int[] second = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] third = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        for (int i = 0; i < answers.length; i++){
+            if (first[i % first.length] == answers[i]) count1++;
+            if (second[i % second.length] == answers[i]) count2++;
+            if (third[i % third.length] == answers[i]) count3++;
+        }
+        
+        int max = Math.max(count1, Math.max(count2, count3));
+        List<Integer> temp = new ArrayList<>();
+        if (count1 == max) temp.add(1);
+        if (count2 == max) temp.add(2);
+        if (count3 == max) temp.add(3);
+        
+        int[] answer = temp.stream().mapToInt(i->i).toArray();
+        
         return answer;
     }
 }
