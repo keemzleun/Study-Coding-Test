6c89de4fa3bc22bd8d53fd50ee0eec82c792a392
김지은 <92363679+keemzleun@users.noreply.github.com>
2025-12-14 12:26:07 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.93 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,24 +1,25 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        
-        int front = arr[0];
-        temp.add(front);
-        for (int i=1; i<arr.length; i++){
-            if (front != arr[i]){
-                front = arr[i];
-                temp.add(arr[i]);
+    public int[] solution(int[] arr) {
+        Queue<Integer> q = new LinkedList<>();
+
+        int last = arr[0];
+        q.add(last);
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != last) {
+                q.add(arr[i]);
+                last = arr[i];
             }
         }
-        
-        int[] answer = new int[temp.size()];
-        for (int i=0; i<temp.size(); i++){
-            answer[i] = temp.get(i);
+
+        int[] answer = new int[q.size()];
+        int idx = 0;
+        while (!q.isEmpty()) {
+            answer[idx++] = q.poll();
         }
-        
-        
+
         return answer;
     }
 }
