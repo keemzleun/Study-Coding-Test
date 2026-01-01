8d485422d5df13967656ea762b38e6bb9bcdb429
김지은 <92363679+keemzleun@users.noreply.github.com>
2026-01-01 19:48:54 +0900

    [level 3] Title: N으로 표현, Time: 27.06 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -3,34 +3,37 @@ import java.util.*;
 class Solution {
     public int solution(int N, int number) {
         if (N == number) return 1;
+        ArrayList<HashSet<Integer>> list = new ArrayList<>();
         
-        List<Set<Integer>> countList = new ArrayList<>();
         for (int i = 0; i <= 8; i++){
-            countList.add(new HashSet<>());
+            list.add(new HashSet<>());
         }
         
         int temp = N;
-        for (int i = 1; i <= 8; i++){
-            countList.get(i).add(temp);
+        for (int i=1; i<=8; i++){
+            list.get(i).add(temp);
             temp = temp * 10 + N;
         }
         
-        for(int i = 1; i <= 8; i++){
+        for (int i = 1; i <= 8; i++){
             for (int j = 1; j < i; j++){
-                for (int val1 : countList.get(j)){
-                    for (int val2 : countList.get(i-j)){
-                        countList.get(i).add(val1 + val2);
-                        countList.get(i).add(val1 - val2);
-                        countList.get(i).add(val1 * val2);
-                        if (val2 != 0) countList.get(i).add(val1/val2);
+                for (int val1 : list.get(j)){
+                    for (int val2 : list.get(i-j)){
+                        list.get(i).add(val1 + val2);
+                        list.get(i).add(val1 - val2);
+                        list.get(i).add(val1 * val2);
+                        if (val2 != 0){
+                            list.get(i).add(val1 / val2);
+                        }
                     }
                 }
             }
-
-            if (countList.get(i).contains(number)) {
+            if (list.get(i).contains(number)){
                 return i;
             }
         }
+        
+        
         return -1;
     }
 }
