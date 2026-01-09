4976b8d22b3c99acfb873cdfe4ed8c8982904c81
김지은 <92363679+keemzleun@users.noreply.github.com>
2026-01-09 10:18:52 +0900

    [level 2] Title: 숫자의 표현, Time: 0.20 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,12 +1,6 @@
-// 자연수 N을 연속된 자연수로 표현하는 방법은 N의 약수 중 홀수의 개수와 같음
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for(int i=1; i<=n; i++){
-            if (n % i == 0 && i % 2 == 1){
-                answer++;
-            }
-        }       
         return answer;
     }
 }
