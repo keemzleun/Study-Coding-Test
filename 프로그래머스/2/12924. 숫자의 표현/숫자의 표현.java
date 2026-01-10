1e5b2d2010dec29499be36185692bfdc2150c63a
김지은 <92363679+keemzleun@users.noreply.github.com>
2026-01-10 12:32:11 +0900

    [level 2] Title: 숫자의 표현, Time: 0.42 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,6 +1,21 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int start = 1, end = 1;
+        int sum = 0;
+        while(start <= n){
+            if (sum < n){
+                sum += end;
+                end++;
+            } else if(sum == n){
+                answer++;
+                sum -= start;
+                start++;
+            } else {
+                sum -= start;
+                start++;
+            }
+        }
         return answer;
     }
 }
