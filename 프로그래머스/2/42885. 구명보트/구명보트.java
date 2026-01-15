60d33dc7231623d00d8402baf6c00ed7ad7c6fc0
김지은 <92363679+keemzleun@users.noreply.github.com>
2026-01-15 20:37:19 +0900

    [level 2] Title: 구명보트, Time: 13.22 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-        
         Arrays.sort(people);
         
         int start = 0;
