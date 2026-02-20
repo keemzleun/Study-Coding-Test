ef9f0df41f83ce74bdc7b56da4fb54049547f371
김지은 <92363679+keemzleun@users.noreply.github.com>
2026-02-20 10:29:59 +0900

    [level 3] Title: 최고의 집합, Time: 0.32 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -3,17 +3,18 @@ class Solution {
         if (n > s){
             return new int[]{-1};
         }
-        
         int[] answer = new int[n];
         
-        int quotient = s / n;
-        for (int i = 0; i < answer.length; i++){
-            answer[i] = quotient;
+        int value = s / n;
+        for(int i=0; i<n; i++){
+            answer[i] = value;
         }
         
         int remainder = s % n;
-        for (int i = 0; i < remainder; i++){
-            answer[n - 1 - i]++;
+        if (remainder != 0){
+            for(int i=0; i<remainder; i++){
+                answer[n-1-i]++;
+            }
         }
         
         return answer;
